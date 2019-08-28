66e6c09f4f3d1a0ad3bff426df2ae5f44ef942f9
Manan <manantaneja007@gmail.com>
2019-08-28 22:17:21 +0530

    Added instructions to install and capture data from table

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # CropPredictionSystem
+
+## Connecting to the database
+
+Traverse to backend folder in your terminal and run the following commands
+
+1. npm run install_dep
+2. npm run install_dev
+3. npm run server
--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 const express = require('express');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'me',
-    password: 'secret',
-    database: 'mydb'
+    host: 'oraclexe.amritanet.edu',
+    user: 'cse17602',
+    password: 'oracle123',
+    database: 'cse17602' // Insert Database name here
 })
 
 connection.connect((err) => {
@@ -13,6 +13,16 @@ connection.connect((err) => {
         throw err;
     }
     console.log("Database connected bitches!! Enjoy!");
+
+})
+
+app.get('/getdata', (req, res) => {
+    let sql = 'select * from course;' // can put any table name here, basically just a mysql query 
+    let query = connection.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send("Data captured from table course");
+    })
 })
 
 const app = express();
